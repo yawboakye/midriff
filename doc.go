c5a15d26771db03f96832f676d018b1027bd8d16
Yaw Boakye <wheresyaw@gmail.com>
2018-12-04 23:26:22 +0100

    package doc + go-style copyright declaration

--- doc.go
+++ doc.go
@@ -1,2 +1,11 @@
-// Package midriff ...
+// Copyright 2018 Yaw Boakye. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package midriff implements HTTP middleware.
+It provides facility for accummulating
+functions into a middleware group, as well
+ as re-using one group in another.
+*/
 package midriff
